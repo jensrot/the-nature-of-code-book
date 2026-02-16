6d156278de2a01d1cf4fde4fbdab239a526032e2
jensrot <jens.rottiers123@gmail.com>
2026-02-17 00:15:22 +0100

    16/02/2026 - chapter_02_vectors

---
+++ chapter_1_vectors/no_vectors_ball/without_vectors/sketch.js
@@ -0,0 +1,31 @@
+let x = 100;
+let y = 100;
+let xspeed = 2.5;
+let yspeed = 2;
+
+function setup() {
+    createCanvas(640, 240);
+    background(255);
+}
+
+function draw() {
+    background(255);
+
+    // Move the ball according to its speed.
+    x = x + xspeed;
+    y = y + yspeed;
+
+    //{!6} Check for bouncing.
+    if (x > width || x < 0) {
+        xspeed = xspeed * -1;
+    }
+    if (y > height || y < 0) {
+        yspeed = yspeed * -1;
+    }
+
+    stroke(0);
+    fill(127);
+    strokeWeight(2);
+    //{!1} Draw the ball at the position (x,y).
+    circle(x, y, 48);
+}
